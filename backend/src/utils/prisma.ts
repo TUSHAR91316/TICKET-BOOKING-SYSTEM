75b50b537696212cb576a49d1610a5ff81b0c2bf
TUSHAR CHOUDHARY <tushar123913@gmail.com>
2025-12-11 10:22:34 +0530

    feat: Add Prisma ORM, define initial data models, and configure project dependencies.

--- backend/src/utils/prisma.ts
+++ backend/src/utils/prisma.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 
+console.log('Env loaded:', process.env.DATABASE_URL);
 const prisma = new PrismaClient();
 
 export default prisma;
